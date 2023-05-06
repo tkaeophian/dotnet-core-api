1eff5c64245ef099dfae7b04e6171c1cb68712e7
Tee Kaeophian <tee@tees-mbp.home>
2023-05-06 17:45:18 +0100

    set default api without version number

--- src/Sample.Api/Controllers/v1/WeatherForecastController.cs
+++ src/Sample.Api/Controllers/v1/WeatherForecastController.cs
@@ -4,6 +4,7 @@ using Sample.Api.Models;
 namespace Sample.Api.Controllers.v1;
 
 [ApiController]
+[Route("api/[controller]")]
 [Route("api/v{version:apiVersion}/[controller]")]
 [ApiVersion("1.0")]
 public class WeatherForecastController : ControllerBase
